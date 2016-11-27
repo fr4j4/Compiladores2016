329d8dcf2aa77842cb3556e2cf019c557365c117
Francisco González <fgonz@outlook.com>
2016-11-27 20:30:16 -0300

    a

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,19 @@
 import java.io.*;
+import java.util.*;
 public class Test{
 	public static void main(String[] args) {
+	    Lexer l;
+	    parser p;
 		try{
-	System.out.println("HelloWorld\nHello Again\nAnd Again");
-	parser p = 
-         new parser (
-           new Lexer (
-              new PushbackReader(new InputStreamReader(System.in), 1024)));
-    Object result = p.parse().value;
-
-    } catch (Exception e) {
-
-      e.printStackTrace();
-	}
+			System.out.println("==] AZ-2016 [==");
+			System.out.println("Todos los derechos reservados");
+			System.out.println("Preparado para recibir instrucciones =)");
+		    l=new Lexer(new PushbackReader(new InputStreamReader(System.in), 1024));
+		    p=new parser(l);
+		    Object o=p.parse();
+		    System.out.println("FIN");
+	    } catch (Exception e) {
+	      e.printStackTrace();
+		}
 	}
 }
